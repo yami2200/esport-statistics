90d4c5cb19a72c7127e33397b28b9446d5021ead
yami2200 <TheYami22@gmail.com>
2023-03-07 09:47:37 +0100

    add world championship example.

--- scripts/script-test.py
+++ scripts/script-test.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-#response = requests.get('https://liquipedia.net/leagueoflegends/api.php?action=query&titles=Faker&prop=revisions&rvprop=content&format=json')
-response = requests.get('https://liquipedia.net/rocketleague/api.php?action=query&titles=Vatira&prop=revisions&rvprop=content&format=json')
+response = requests.get('https://liquipedia.net/leagueoflegends/api.php?action=query&titles=World_Championship/2022&prop=revisions&rvprop=content&format=json')
+#response = requests.get('https://liquipedia.net/rocketleague/api.php?action=query&titles=Vatira&prop=revisions&rvprop=content&format=json')
 data = response.json()
 
-with open("results/output-test-vatira.json", "w") as output_file:
+with open("results/output-test-world.json", "w") as output_file:
     json.dump(data, output_file)
