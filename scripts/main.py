15367d6d5c4fee9cd5f9afbeb35827542337bb69
yami2200 <TheYami22@gmail.com>
2023-03-21 12:01:02 +0100

    Add results to gitignore. Commit CS script

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,14 @@
 from lol import *
+from cs import *
 from utilities import *
 
 #######   Configuration   #######
-mode = ParsingMode.NO_FETCHING
-games = ["lol", "rl", "cs"]
+mode = ParsingMode.READ_FIRST_ALL
+games = ["rl", "cs"]
 
 #######   Main   #######
 print("Starting collecting data")
 for game in games:
     if game == "lol": run_lol(mode)
     elif game == "rl": print("Rocket League is not supported yet")
-    elif game == "cs": print("Counter Strike is not supported yet")
+    elif game == "cs": run_cs(mode)
